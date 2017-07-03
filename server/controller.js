877f1260efaab91718ab487335b8df71cfc40c5b
Andrew Zhang <Andrew.Zhang@emc.com>
2017-07-03 15:09:43 +0800

    add trend handler

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,5 @@
 import Express from 'express';
+import { getTrend } from './trendHandler';
 import { overviewGroup, hireGroup, detailGroup, trendGroup, forecastGroup, tableHeader, tableContent } from './mock';
 
 const apiRoutes = Express.Router();
@@ -6,5 +7,14 @@ const apiRoutes = Express.Router();
 apiRoutes.get('/dashboard', (req, res) => {
   res.json({ overviewGroup, hireGroup, detailGroup, trendGroup, forecastGroup, tableHeader, tableContent });
 });
+
+apiRoutes.get('/trend', (req, res) => {
+  let trend = getTrend();
+  res.json(trend);
+});
+
+
+
+getTrend();
 
 export default apiRoutes;
