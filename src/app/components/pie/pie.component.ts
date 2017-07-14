08b4a7a32301442646cd7a8d99221a5b16f0afc6
Andrew Zhang <Andrew.Zhang@emc.com>
2017-07-14 17:06:11 +0800

    change pie chart lable

--- src/app/components/pie/pie.component.ts
+++ src/app/components/pie/pie.component.ts
@@ -1,15 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'pie',
   templateUrl: './pie.component.html',
 })
-export class PieComponent {
+export class PieComponent implements OnChanges{
   @Input() pieData: any[];
 
   view: any[] = [600, 300];
-
+  total: number;
   // options
   showLegend = false;
 
@@ -24,5 +24,12 @@ export class PieComponent {
 
   ngOnInit() {
 
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    for (let propName in changes) {
+      let chng = changes[propName];
+      let cur  = chng.currentValue;
+      this.total = cur.reduce((a,b) => a + b.value, 0);
+    }
   }
 }
