c114c5d22a3cc3fe7e5b7dba865828c01956254d
CORP\hel11 <lisa.he@emc.com>
2017-10-11 17:14:38 +0800

    add a trend line represent on board number

--- server/handlers/trendHandler.js
+++ server/handlers/trendHandler.js
@@ -1,7 +1,7 @@
 import xlsx from 'node-xlsx';
 import fs from 'fs';
 
-import { genterateWeekDomain } from './utils/tools'
+import { genterateWeekDomain, getTargetColumn, getLastSunday, reduceByDate } from './utils/tools'
 import { EXPECT_OFFER_TREND, DATE_RANGE } from './utils/constants';
 
 const filePath = {
@@ -9,12 +9,17 @@ const filePath = {
   ecs: require('path').resolve(__dirname, '../assets/ecs-offer-trend'),
 }
 
+const reqTrackFilePath = {
+  isilon: require('path').resolve(__dirname, '../assets/Isilon Hiring Req Track Sheet.xlsx'),
+  ecs: require('path').resolve(__dirname, '../assets/ECS Hiring Plan.xlsx')
+}
+
 export const getTrend = (project) => {
   let dateRange = genterateWeekDomain(DATE_RANGE[project].START, DATE_RANGE[project].END);
   return {
     reqExpect: getCombined(dateRange, EXPECT_OFFER_TREND[project]),
-    // reqReal: getCombined(dateRange, getRealOfferArray(project)),
-    // onboardReal: getCombined(dateRange, getRealOnboardArray(project))
+    //reqReal: getCombined(dateRange, getRealOfferArray(project)),
+    onboardReal: getCombined(dateRange, getRealOnboardArray(project))
   }
 }
 
@@ -32,5 +37,42 @@ function getRealOfferArray(project) {
 }
 
 function getRealOnboardArray(project) {
+  if(project === 'overview'){
+    let isilonOnboardArray = getProjectOnboardArray('isilon');
+    let ecsOnboardArray = getProjectOnboardArray('ecs');
+    return isilonOnboardArray.map((num, idx)=>{
+      return num + ecsOnboardArray[idx];
+    })
+  }
+  else{
+    return getProjectOnboardArray(project);
+  }
+}
 
+function getProjectOnboardArray(project){
+  const reqRawData = xlsx.parse(reqTrackFilePath[project])[1].data;
+  let onBoardRawData = getTargetColumn(reqRawData, 'On Board Date');
+  let customizedOnBoardData = getLastSunday(onBoardRawData.sort());
+  let reducedOnboardData = reduceByDate(customizedOnBoardData);
+  let dateToday = new Date();
+  let dateRange = genterateWeekDomain(DATE_RANGE[project].START, dateToday);
+  let realOnboardArray = [];
+  dateRange.forEach(function(date, finalIndex){
+    let dateIndex = reducedOnboardData.findIndex(dateValuePair=>{
+      return dateValuePair.name === date;
+    })
+    if(dateIndex>=0){
+      realOnboardArray.push(reducedOnboardData[dateIndex].value);
+    }
+    else{
+      if(finalIndex!==0){
+        realOnboardArray.push(realOnboardArray[finalIndex-1]);
+        
+      }else{
+        realOnboardArray.push(0);
+      }
+      
+    }
+  })
+  return realOnboardArray;
 }
